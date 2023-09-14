d40b3e016c96b4654c2073e0aa8bd7638c767c32
João Vítor <jjoaovitor7@outlook.com.br>
2023-09-14 18:52:43 -0300

    aula 3.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
   plugins: [react()],
   resolve: {
     alias: {
